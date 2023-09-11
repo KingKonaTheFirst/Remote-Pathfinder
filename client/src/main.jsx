2f56d7cb9f02893f0e047993bca95614a36b84d9
rdheadyii <rdheadyii@gmail.com>
2023-09-11 12:39:59 -0400

    made starter pages and wrote placeholder code/rewrote main.jsx/added folders to src folder

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import App from "./App.jsx";
+import App from './App.jsx';
+import Error from './pages/Error.jsx';
+import Home from './pages/Home.jsx';
+import Contact from './pages/Contact.jsx';
+import Login from './pages/Login.jsx';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/signup',
+        element: <Signup />
+      }, {
+        path: '/contact',
+        element: <Contact />
+      }, {
+        path: '/profiles',
+        element: <Profile />
+      }
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<RouterProvider router={router} />
 );
