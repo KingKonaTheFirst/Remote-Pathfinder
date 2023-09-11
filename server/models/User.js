0cab8b743680c0a3acfafaccb81d6d2e9e4de629
Travis <travis3214@yahoo.com>
2023-09-11 11:14:07 -0400

    created a model for users

---
+++ server/models/User.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+const Job = require('./jobs');
+
+const userSchema = new Schema({
+
+  first: {
+    type: String,
+    required: true,
+    max_length: 50,
+  },
+  last: {
+    type: String,
+    required: true,
+    max_length: 50,
+  },
+  email: {
+    type: String,
+    require: true, 
+    unique: true,
+  },
+  phone: {
+    type: String,
+  },
+  skills: {
+    type: String, 
+    required: true,
+    max_length: 500,
+  },
+  expected_pay: {
+    type: Number,
+    max_length: 10,
+  },
+  savedjobs: [Job],
+
+})
+
+userSchema
+  .virtual('fullName')
+  // Getter
+  .get(function () {
+    return `${this.first} ${this.last}`;
+  })
+  // Setter to set the first and last name
+  .set(function (v) {
+    const first = v.split(' ')[0];
+    const last = v.split(' ')[1];
+    this.set({ first, last });
+  });
+
+// Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
