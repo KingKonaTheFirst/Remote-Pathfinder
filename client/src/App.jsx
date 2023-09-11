c19ac218b4b243114543af259db5ec9b9dc19317
rdheadyii <rdheadyii@gmail.com>
2023-09-11 12:13:24 -0400

    rewrote App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,40 @@
-function App() {
-  const [count, setCount] = useState(0);
+import { Outlet } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import Nav from './components/Nav';
 
-  return <h1>Hey</h1>;
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+    <div>
+        <Nav />
+        <Outlet />
+    </div>
+  </ApolloProvider>
+  );
 }
 
 export default App;
