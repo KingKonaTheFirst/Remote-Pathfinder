5a106480ba7af06b57a51ba12fc62204f1ed1255
Kona <landon.deroin@gmail.com>
2023-09-11 11:14:47 -0400

    fixed the resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,24 +1,23 @@
-const express = require("express");
-const router = express.Router();
 const Job = require("../models/Job.js");
 const Employer = require("../models/Employer");
-// query for jobs
-router.get("/api/jobs", async (req, res) => {
-  try {
-    const jobs = await Job.find({});
-    res.json(jobs);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
 
-router.get("/api/employers", async (req, res) => {
-  try {
-    const employers = await Employer.find({}).populate("jobs");
-    res.json(employers);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+const resolvers = {
+  Query: {
+    jobs: async () => {
+      try {
+        return await Job.find({});
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    employers: async () => {
+      try {
+        return await Employer.find({}).populate("jobs");
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+};
 
-module.exports = router;
+module.exports = resolvers;
