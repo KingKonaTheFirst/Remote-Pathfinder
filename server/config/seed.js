68cdcb1a55ac527ff1aaba88ade2b1adbdbec137
rdheadyii <rdheadyii@gmail.com>
2023-09-12 11:09:53 -0400

    wrote more user seeds

--- server/config/seed.js
+++ server/config/seed.js
@@ -5,7 +5,25 @@ db.once('open', async () => {
     await User.create({
         first: 'Eric',
         last: 'Graves',
-        email: 'egravesboy@testmail.com',
+        email: 'thegraveyboy@testmail.com',
         password: 'password12345',
     });
+
+    await User.create({
+        first: 'Samuel',
+        last: 'Cordova',
+        email: 'cordovanova@testmail.com',
+        password: 'password12345',
+    });
+
+    await User.create({
+        first: 'Brittani',
+        last: 'Court',
+        email: 'crittanibourt@testmail.com',
+        password: 'password12345',
+    });
+
+    console.log('users seeded');
+
+    process.exit();
 });
