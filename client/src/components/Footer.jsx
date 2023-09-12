19f075202349b1243718f16727bbbb79f01f9d7a
Travis <travis3214@yahoo.com>
2023-09-12 14:02:31 -0400

    added logo to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import '/index.css'
+import {FaGithub} from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="">
-        Footer
+    <div className="footer1">
+      <h3 className="footer1">
+        Remote Pathfinder &copy; 2023
+      </h3>
+      <div className="footer2">
+        <div>
+          <a className="link" href="#">About</a>
+        </div>
+        <div>
+        <a className="link" href="#">Help</a>
+        </div>
       </div>
-    </footer>
+      <div className="footer2">
+        <a className="link" href="https://github.com/KingKonaTheFirst/Remote-Pathfinder" target="_blank"><FaGithub /></a>
+      </div>
+    </div>
   );
 };
 
