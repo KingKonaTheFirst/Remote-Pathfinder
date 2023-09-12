bc024a476b13c857b0263deefde6b1fca0062abc
rdheadyii <rdheadyii@gmail.com>
2023-09-12 14:03:15 -0400

    started contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,4 +1,23 @@
+import { useState } from 'react';
+import Input from "../components/Input";
+
 const Contact = () => {
+
+    const [input, setInput] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+    });
+
+    const handleChange = (e) => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value,
+        });
+        console.log("tracking");
+    };
+
     return (
         <div>
             <h1>Contact</h1>
