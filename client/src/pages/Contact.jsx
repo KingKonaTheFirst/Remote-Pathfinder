406f8284b6cdeea5b838c0810513399f4fde77f3
rdheadyii <rdheadyii@gmail.com>
2023-09-12 14:49:15 -0400

    added more to contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -18,9 +18,30 @@ const Contact = () => {
         console.log("tracking");
     };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(input);
+        setInput({
+            name: "",
+            email: "",
+            subject: "",
+            message: "",
+        });
+    };
+
     return (
         <div>
             <h1>Contact</h1>
+            <form action="" onSubmit={handleSubmit}>
+
+                <Input event={handleChange}/>
+
+                <Input event={handleChange}/>
+
+                <Input event={handleChange}/>
+
+                <textarea name="" id="" cols="30" rows="10" onChange={handleChange}></textarea>
+            </form>
         </div>
     );
 };
