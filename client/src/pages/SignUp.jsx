5990044f820546cbaa900c27ad4b3101d3ccc134
WyattBensman <wyattbensman5@gmail.com>
2023-09-13 13:17:09 -0400

    Added Backend Functionality to Frontend of SignUp Form

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,85 +1,141 @@
 import { useState } from "react";
 
 export default function UserSignUp() {
-  const [first, setFirst] = useState("");
-  const [last, setLast] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [password, setPassword] = useState("");
+  // State to Hold Form Data
+  const [signUpData, setSignUpData] = useState({
+    first: "",
+    last: "",
+    email: "",
+    password: "",
+    phone: "",
+  });
+
+  // Mutation Hook
+  const [createUser] = useMutation(CREATE_USER);
+
+  // Handle input changes and update form data
+  const handleInputChange = (e) => {
+    setSignUpData({ ...signUpData, [e.target.name]: e.target.value });
+  };
+
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { data } = await createUser({
+        variables: { ...signUpData },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <div className="p-5 flex justify-center items-center">
-      <form className="w-full max-w-lg bg-gray-800 p-8 rounded-lg">
-        <h1 className="mb-6 text-3xl font-medium text-white text-center">Sign Up</h1>
-        
+    <div className="flex items-center justify-center p-5">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-lg p-8 bg-gray-800 rounded-lg"
+      >
+        <h1 className="mb-6 text-3xl font-medium text-center text-white">
+          Sign Up
+        </h1>
+
         <div className="flex gap-4">
           <div className="flex-1">
-            <label htmlFor="first" className="block text-lg font-medium text-white">First Name:</label>
+            <label
+              htmlFor="first"
+              className="block text-lg font-medium text-white"
+            >
+              First Name:
+            </label>
             <input
-              value={first}
               name="first"
-              onChange={(e) => setFirst(e.target.value)}
+              value={signUpData.first}
+              onChange={handleInputChange}
               type="text"
               placeholder="Enter your first name"
-              className="mt-2 w-full px-3 py-2 text-gray-900 bg-white rounded shadow focus:outline-none focus:border-blue-500"
+              className="w-full px-3 py-2 mt-2 text-gray-900 bg-white rounded shadow focus:outline-none focus:border-blue-500"
             />
           </div>
 
           <div className="flex-1">
-            <label htmlFor="last" className="block text-lg font-medium text-white">Last Name:</label>
+            <label
+              htmlFor="last"
+              className="block text-lg font-medium text-white"
+            >
+              Last Name:
+            </label>
             <input
-              value={last}
+              value={signUpData.last}
               name="last"
-              onChange={(e) => setLast(e.target.value)}
+              onChange={handleInputChange}
               type="text"
               placeholder="Enter your last name"
-              className="mt-2 w-full px-3 py-2 text-gray-900 bg-white rounded shadow focus:outline-none focus:border-blue-500"
+              className="w-full px-3 py-2 mt-2 text-gray-900 bg-white rounded shadow focus:outline-none focus:border-blue-500"
             />
           </div>
         </div>
 
         <div className="mt-6">
-          <label htmlFor="email" className="block text-lg font-medium text-white">Email Address:</label>
+          <label
+            htmlFor="email"
+            className="block text-lg font-medium text-white"
+          >
+            Email Address:
+          </label>
           <input
-            value={email}
+            value={signUpData.email}
             name="email"
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={handleInputChange}
             type="email"
             placeholder="example@gmail.com"
-            className="mt-2 w-full px-3 py-2 text-gray-900 bg-white rounded shadow focus:outline-none focus:border-blue-500"
+            className="w-full px-3 py-2 mt-2 text-gray-900 bg-white rounded shadow focus:outline-none focus:border-blue-500"
           />
         </div>
 
         <div className="flex gap-4 mt-6">
           <div className="flex-1">
-            <label htmlFor="password" className="block text-lg font-medium text-white">Password:</label>
+            <label
+              htmlFor="password"
+              className="block text-lg font-medium text-white"
+            >
+              Password:
+            </label>
             <input
-              value={password}
+              value={signUpData.password}
               name="password"
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={handleInputChange}
               type="password"
               placeholder="Enter your password"
-              className="mt-2 w-full px-3 py-2 text-gray-900 bg-white rounded shadow focus:outline-none focus:border-blue-500"
+              className="w-full px-3 py-2 mt-2 text-gray-900 bg-white rounded shadow focus:outline-none focus:border-blue-500"
             />
           </div>
 
           <div className="flex-1">
-            <label htmlFor="phone" className="block text-lg font-medium text-white">Phone #:</label>
+            <label
+              htmlFor="phone"
+              className="block text-lg font-medium text-white"
+            >
+              Phone #:
+            </label>
             <input
-              value={phone}
+              value={signUpData.phone}
               name="phone"
-              onChange={(e) => setPhone(e.target.value)}
+              onChange={handleInputChange}
               type="text"
               placeholder="Enter your phone number"
-              className="mt-2 w-full px-3 py-2 text-gray-900 bg-white rounded shadow focus:outline-none focus:border-blue-500"
+              className="w-full px-3 py-2 mt-2 text-gray-900 bg-white rounded shadow focus:outline-none focus:border-blue-500"
             />
           </div>
         </div>
 
-        <div 
-  className="w-full mt-8 px-4 py-2 font-medium text-white overflow-hidden rounded cursor-pointer hover:teal-800 focus:outline-none focus:bg-teal-900 transition-color">
-  Submit
-</div>
+        <button
+          type="submit"
+          className="w-full px-4 py-2 mt-8 overflow-hidden font-medium text-white rounded cursor-pointer hover:teal-800 focus:outline-none focus:bg-teal-900 transition-color"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
