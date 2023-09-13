4737367f05217342b51241cbe6279de5219a1996
Travis <travis3214@yahoo.com>
2023-09-12 20:13:50 -0400

    fixed footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,20 +6,21 @@ import "../index.css"
 const Footer = () => {
   return (
     <div className="footer1">
-      <h3 className="footer1">
-        Remote Pathfinder &copy; 2023
-      </h3>
       <div className="footer2">
         <div>
           <Link className="link" to='/About'>About</Link>
+        </div>
+        <div className="footer2">
+        <a className="link" href="https://github.com/KingKonaTheFirst/Remote-Pathfinder" target="_blank"><FaGithub /></a>
         </div>
         <div>
         <Link className="link" to='/Contact'>Contact</Link>
         </div>
       </div>
-      <div className="footer2">
-        <a className="link" href="https://github.com/KingKonaTheFirst/Remote-Pathfinder" target="_blank"><FaGithub /></a>
-      </div>
+      
+      <h3 className="footer1">
+        Remote Pathfinder &copy; 2023
+      </h3>
     </div>
   );
 };
