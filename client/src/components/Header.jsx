6e2a6ba248201477d3ea9f00378ece24802c690b
Kona <landon.deroin@gmail.com>
2023-09-13 14:59:32 -0400

    i have no idea what i changed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -48,7 +48,7 @@ const Header = () => {
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
           {/* logo */}
-          <a href="#">
+          <a to="/">
             <img src={Logo} alt="Logo" />
           </a>
 
