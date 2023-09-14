dda3449ef266f1879f240da616a49c049a63302a
Travis <travis3214@yahoo.com>
2023-09-14 19:26:34 -0400

    finished the profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -40,29 +40,31 @@ const Profile = () => {
                             ))}
                         </ul>
                         <input
-                        className='input'
+                            className='input'
                             type="text"
                             id="txtVal"
                             placeholder="Enter a skill"
                             value={inputValue}
                             onChange={(e) => setInputValue(e.target.value)}
                         />
-                        <button className='button' onClick={addSkill}>Add Skill</button>
+                        <button className='button' onClick={addSkill}>Add+</button>
                     </div>
-                    <div className="flex-1 text-center bg-gray-500 px-4 py-2 m-2">
+                    <div className="flex-1 text-black text-center bg-gray-500 px-4 py-2 m-2">
                         <h3 className='heading2 font-bold'>Resume</h3>
-                        <div className='flex justify-center' >
-                            {resume && <p>Uploaded Resume: {resume.name}</p>}
+                        {resume && (
+                            <div>
+                                <p>Uploaded Resume: {resume.name}</p>
+                                <a href={URL.createObjectURL(resume)} download={resume.name}>Download Resume</a>
+                            </div>
+                        )}
 
-                            {/* File upload input (moved under the "Resume" section) */}
-                            <input
-                                className='resume'
-                                type="file"
-                                accept=".pdf,.doc,.docx"
-                                onChange={handleFileUpload}
-                                style={{ color: 'black' }}
-                            />
-                        </div>
+                        {/* File upload input */}
+                        <input
+                            className='resume'
+                            type="file"
+                            accept=".pdf,.doc,.docx"
+                            onChange={handleFileUpload}
+                        />
                     </div>
                 </div>
             </div>
