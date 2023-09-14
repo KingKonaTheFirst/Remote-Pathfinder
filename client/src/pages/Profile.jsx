08efb08ea845c185314c03f00ab13ffddf8d2b26
Travis <travis3214@yahoo.com>
2023-09-14 13:48:15 -0400

    fixed alignment of remove button

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -23,11 +23,11 @@ const Profile = () => {
             <h1 className="heading">Profile</h1>
             <div>
                 <div className="flex bg-gray-400 profile">
-                    <div className="flex-1 text-gray-800 text-center bg-gray-400 px-4 py-2 m-2">
+                    <div className="flex-1 text-center bg-gray-400 px-4 py-2 m-2">
                         <h3 className='heading2 font-bold' >Skills</h3> 
                         <ul id="list">
                             {skills.map((skill, index) => (
-                                <li key={index}>
+                                <li className='flex justify-center' key={index}>
                                     {skill}
                                     <button className='listItem' onClick={() => removeSkill(index)}><FaBackspace /></button>
                                 </li>
