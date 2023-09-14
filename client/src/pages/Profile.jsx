27687b69f9e7ef3dc24ca8c4e43b5daf097bbd8f
Travis <travis3214@yahoo.com>
2023-09-14 13:22:53 -0400

    added remove button for skills

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,41 +1,50 @@
 import React, { useState } from 'react';
+import { FaBackspace } from "react-icons/fa";
 
 const Profile = () => {
-    // Use React state to manage the list of skills
     const [skills, setSkills] = useState([]);
     const [inputValue, setInputValue] = useState('');
 
     const addSkill = () => {
         if (inputValue.trim() !== '') {
             setSkills([...skills, inputValue]);
-            setInputValue(''); // Clear the input field after adding a skill
+            setInputValue('');
         }
+    };
+
+    const removeSkill = (index) => {
+        const updatedSkills = [...skills];
+        updatedSkills.splice(index, 1);
+        setSkills(updatedSkills);
     };
 
     return (
         <div className="text-center">
             <h1 className="heading">Profile</h1>
             <div>
-                <div className="about">Placeholder text</div>
                 <div className="flex bg-gray-400 profile">
-                    <div className="flex-1 text-gray-800 text-center bg-gray-500 px-4 py-2 m-2">
-                        Skills 
+                    <div className="flex-1 text-gray-800 text-center bg-gray-400 px-4 py-2 m-2">
+                        <h3 className='heading2 font-bold' >Skills</h3> 
                         <ul id="list">
                             {skills.map((skill, index) => (
-                                <li key={index}>{skill}</li>
+                                <li key={index}>
+                                    {skill}
+                                    <button className='listItem' onClick={() => removeSkill(index)}><FaBackspace /></button>
+                                </li>
                             ))}
                         </ul>
                         <input
+                        className='input'
                             type="text"
                             id="txtVal"
                             placeholder="Enter a skill"
                             value={inputValue}
                             onChange={(e) => setInputValue(e.target.value)}
                         />
-                        <button onClick={addSkill}>Add Skill</button>
+                        <button className='button' onClick={addSkill}>Add Skill</button>
                     </div>
                     <div className="flex-1 text-gray-800 text-center bg-gray-500 px-4 py-2 m-2">
-                        Resume
+                        <h3 className='heading2 font-bold'>Resume</h3>
                     </div>
                 </div>
             </div>
